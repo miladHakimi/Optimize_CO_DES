0413fa441a9b4fb5b0cdc4d139e234fb7851e89e
miladHakimi <miladhakimi33@gmail.com>
2019-06-26 16:29:15 +0430

    W1 matrix change.

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ long long int sigmoid(long long int x)
 //Profiling.1.end
 
 //Profiling.2.start
-int predict(int *inputs)
+int predict(int *inputs, long long int temp2[10][128])
 {
 	int scaleFactor = 1 << 15, p = 0;
 	int i,j,res;
@@ -42,13 +42,14 @@ int predict(int *inputs)
 	long long int temp1=0;
 	//First Layer
 	//Profiling.3.start
-	// todo: change matrix
+
 	for (i = 0; i < 10; i++)
 	{
 		temp1 = 0;
-		for (j = 0; j < 128; j++)
+		for (j = 0; j < 128; j++){
 			//Profiling.6.start
-			temp1 += W1[j][i] * inputs[j];	//MAC Instruction
+			temp1 += temp2[i][j] * inputs[j];	//MAC Instruction
+		}
 
 		temp1 = temp1 / scaleFactor;
 		temp1 += B1[i];
@@ -75,27 +76,31 @@ int predict(int *inputs)
 		p = (temp1>res)*i + (temp1<=res)*p;
 		res = (temp1>res)*temp1 + (temp1<=res)*res;
 	}
-		
 	return p + 1;
 }
 //Profiling.2.end
 
 int main(int argc, char *argv[])
 {
-	int c = 0,i;
+	int c = 0,i,j;
 	double total_time;
     struct timeval  tv1, tv2;
 	gettimeofday(&tv1, NULL);
-
-	for (i = 0; i < 100; i++)
+	
+	long long int temp2[10][128];
+	for (i = 0; i < 128; ++i)
+		for (j = 0; j < 10; ++j)
+			temp2[j][i] = W1[i][j];
+	
+	for (i = 0; i < 10000; i++)
 		//Profiling.5.start
-		if (predict(data[i]) == labels[i])		//Comparing MLP Outputs and Targets
+		if (predict(data[i%100], temp2) == labels[i%100])		//Comparing MLP Outputs and Targets
 			c++;
 		//Profiling.5.end
 
 	gettimeofday(&tv2, NULL);
-    printf ("Total time = %f seconds\n",
-         (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 );
+    printf ("Total time = %f miliseconds\n",
+         (double) (tv2.tv_usec - tv1.tv_usec) / 1000 );
 
 	printf("CCR: %d\n", c);
 	return 0;
